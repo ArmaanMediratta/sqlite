0b4d68896098e5af38329c72a6e791477c3466f4
Armaan Mediratta <armaan@taamba.com>
2026-03-30 11:21:48 -0500

    pager constructor done & cmake edits

--- include/pager.h
+++ include/pager.h
@@ -1,24 +1,41 @@
 #ifndef PAGER_H
 #define PAGER_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
-#define PAGE_SIZE ((uint16_t)4096)
-#define MAX_PAGES ((uint32_t)65535)
+#define PAGE_SIZE  ((uint16_t)4096)
+#define CACHE_SIZE ((uint16_t)64)
+
+typedef enum
+{
+  PAGER_OK,
+  PAGER_ERR_IO,
+  PAGER_ERR_NOMEM,
+  PAGER_ERR_CORRUPT
+} PagerStatus;
 
 typedef struct
 {
-  int fd;             //.db file desc
-  uint32_t num_pages; // num of pages on .db file
-  void* cache;        // cache of pages (simple for now)
+  uint32_t page_no;
+  void* data;
+  bool ref;
+  bool dirty;
+} Page;
+
+typedef struct
+{
+  int fd;                 //.db file desc
+  uint32_t num_pages;     // num of pages on .db file
+  Page cache[CACHE_SIZE]; // cache arr (2nd change algo)
+  uint32_t clock_hand;
 } Pager;
 
-Pager* p_open(const char* filename);
-int p_close(Pager* p);
+PagerStatus p_open(const char* filename, Pager** out);
+PagerStatus p_close(Pager* p);
 
-int read_page(Pager* p, uint32_t page_no, void* page_data);
-int write_page(Pager* p, void* page_data);
-int update_page(Pager* p, uint32_t page_no, void* page_data);
-int free_page(Pager* p, uint32_t page_no);
+PagerStatus p_alloc_page(Pager* p, uint32_t* page_no);
+PagerStatus p_read_page(Pager* p, uint32_t page_no, void** page_data);
+PagerStatus p_write_page(Pager* p, uint32_t page_no, void* page_data);
 
 #endif
--- src/pager.c
+++ src/pager.c
@@ -1,6 +1,72 @@
 #include "pager.h"
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main()
+PagerStatus p_open(const char* filename, Pager** out)
 {
-  return 0;
+  Pager* p = calloc(1, sizeof(Pager));
+  if (p == NULL)
+  {
+    return PAGER_ERR_NOMEM;
+  }
+
+  int fd = open(filename, O_RDWR | O_CREAT, 0644);
+  if (fd == -1)
+  {
+    free(p);
+    return PAGER_ERR_IO;
+  }
+  p->fd = fd;
+
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+  {
+    close(fd);
+    free(p);
+    return PAGER_ERR_IO;
+  }
+
+  if (st.st_size % PAGE_SIZE != 0)
+  {
+    close(fd);
+    free(p);
+    return PAGER_ERR_CORRUPT;
+  }
+
+  if (st.st_size != 0)
+  {
+    p->num_pages = (st.st_size / PAGE_SIZE);
+  }
+  else
+  {
+    p->num_pages = 0;
+  }
+
+  for (int i = 0; i < CACHE_SIZE; ++i)
+  {
+    p->cache[i].page_no = UINT32_MAX;
+  }
+  *out = p;
+
+  return PAGER_OK;
+}
+
+PagerStatus p_close(Pager* p)
+{
+}
+
+PagerStatus p_alloc_page(Pager* p, uint32_t* page_no)
+{
+}
+
+PagerStatus p_read_page(Pager* p, uint32_t page_no, void** page_data)
+{
+}
+
+PagerStatus p_write_page(Pager* p, uint32_t page_no, void* page_data)
+{
 }
